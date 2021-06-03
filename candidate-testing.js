f51533cabfb6f11300865e719acffa57ce161674
amadoucisse <>
2021-06-03 23:03:07 +0000

    check spelling

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", "True or false: 5 Kilometer == 5000 meters? ", "(5 + 3)/2 * 10 =? ", "Given the array [8, 'Orbit', 'Trajectory',45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 Kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory',45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride","true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -34,7 +34,7 @@ let numberOfCorrectAnswers = 0
     if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
       numberOfCorrectAnswers ++
     }
-      console.log(`${i + 1}) ${questions[i]}\nYour answer: ${candidateAnswers[i]}\nCorrect answer: ${correctAnswers[i]}` + '\n');
+      console.log(`${i + 1}) ${questions[i]}\nYour Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}` + '\n');
   }
   let grade =(numberOfCorrectAnswers / questions.length) * 100;
   console.log(`>>>Overall Grade: ${grade}% (${numberOfCorrectAnswers} of ${questions.length} responses correct<<<)`);
